a4de4536d86033cdaadc55252325f02930845df7
j1330 <qnic.ayapons@i.softbank.jp>
2017-12-09 16:30:13 +0900

    update field

--- field.rb
+++ field.rb
@@ -5,12 +5,29 @@ class Field
   attr_reader :open_card, :tableau, :deck
   def initialize
     colors = [:red,:blue,:green,:yellow]
-    numbers = [*0..9]+[*1..9]
+    numbers = [*0..9] + [*1..9]
     @deck = colors.product(numbers).map do |color, num|
               costume = "a"#Image.load(color+num.to_s+".png")
               NumberCard.new(costume, color, num)
             end
+    @tableau = []
+    @deck.shuffle!
+  end
+  def shuffle!
+    @tableau.shuffle!
+  end
+  def refresh
+    self.shuffle!
+    @deck = @tableau + @deck
+    @tableau = []
+  end
+  def pop_cards(n)
+    @deck.pop(n)
+  end 
+  def set_card(card)
+    if @open_card
+      @tableau.push(@open_card)
+    end
+    @open_card = card
   end
 end
-
-Field.new
