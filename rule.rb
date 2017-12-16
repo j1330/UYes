fda4b055c960bbd0fcc49c7e8a9ce494d825f6ee
j1330 <qnic.ayapons@i.softbank.jp>
2017-12-16 10:51:19 +0900

    add judge playable cards
    出せるカードを判定する機能を付けた

--- rule.rb
+++ rule.rb
@@ -1,6 +1,18 @@
+require_relative "number_card.rb"
 module Rule
+  module_function
   def judge_playable_cards(open_card, cards)
     cards.map do | card |
+      if open_card.instance_of?(NumberCard)
+        card.number == open_card.number || card.color == open_card.color
+      end
     end
   end  
 end
+# open = NumberCard.new("a",:red, 7)
+# cards = [
+#   NumberCard.new("a",:blue,4),
+#   NumberCard.new("a",:red,5),
+#   NumberCard.new("a",:green,7)
+# ]
+# puts Rule.judge_playable_cards(open,cards)
